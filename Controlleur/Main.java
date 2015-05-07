4295c174697f65c10636e222343a0756278cab08
Mounier <dridri_du91@hotmail.fr>
2015-05-07 19:16:58 +0200

    Ajout du document Controlleur contenant la class Main

---
+++ Controlleur/Main.java
@@ -0,0 +1,20 @@
+package Controlleur;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+import Vue.Fenetre;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fenetre vue = new Fenetre();
+		vue.setVisible(true); 
+		
+	}
+
+}
